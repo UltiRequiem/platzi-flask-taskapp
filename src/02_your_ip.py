eec2d0b98997211e14b878fb0ffef6f9a412bb24
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-26 18:37:27 -0500

    ✨

---
+++ src/02_your_ip.py
@@ -0,0 +1,11 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    user_ip = request.remote_addr
+    return f'Hello World Platzi, tu IP es{user_ip}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
