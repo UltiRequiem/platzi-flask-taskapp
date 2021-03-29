9ddeeb82b0f45be4029dd254315e0c4c03239973
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-28 22:10:08 -0500

    ✨ Error 500

--- src/main.py
+++ src/main.py
@@ -32,6 +32,11 @@ def zero():
 def not_found(error):
     return render_template('404.html', error=error)
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
