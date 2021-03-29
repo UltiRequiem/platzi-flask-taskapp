d39284e73ab6e1c9a91814bd29c2f4635bd9734b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-28 20:56:48 -0500

    ✨ Update Statics

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request,make_response, redirect, render_template
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
-todos = ['Comprar Cafeina', 'Enviar 2 Solicitud de compra', 'Entregar video a productor']
+todos = ['Comprar Cafeina','Enviar 2 Solicitud de compra', 'Entregar video a productor']
 @app.route('/')
 def index():
     user_ip = request.remote_addr
