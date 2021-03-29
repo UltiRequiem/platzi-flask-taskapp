45f55985ae29858928041e7d3ddfcd642c2266e6
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-29 17:42:23 -0500

    ✨ Flask WTF

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = ' SUPER SECRETO'
 
 todos = ['Comprar Cafeina', 'Enviar 2 Solicitud de compra',
          'Entregar video a productor']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Enter')
 
 
 @app.route('/')
@@ -14,7 +24,7 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/zero'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
@@ -22,11 +32,12 @@ def index():
 @app.route('/zero')
 def zero():
 
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos': todos
-    }
+        'todos': todos,
+        'login_form': login_form}
 
     return render_template('hello.html', **context)
 
